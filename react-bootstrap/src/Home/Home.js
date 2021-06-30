e8a8719e191c01dc67f5180f9307c8e407614b2f
Garin Escorcia-Ariza <garinescorcia@yahoo.com>
2021-06-30 18:18:18 +0200

    update

---
+++ react-bootstrap/src/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home = () => (
+    <section className="jumbotron jumbotron-fluid sec-profile container" id="profile">
+        <div className="container my_profile">
+            <h1 className="text-light text-center">Garin Escorcia Ariza</h1>
+            <h5 className="text-light text-center">Mechatronic Engineer and Ph. D. in Nanotechnology</h5>
+        </div>	
+    </section>
+)
